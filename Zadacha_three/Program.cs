f8386f264f4f90b53358ac0f5c61aba243cd2d7c
dmitry <rbdeman@yandex.ru>
2022-12-18 20:34:10 +0300

    сделал третью задачу

--- Zadacha_three/Program.cs
+++ Zadacha_three/Program.cs
@@ -1,1 +1,62 @@
-﻿
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(leftRange, rightRange);
+        }
+    }
+    return array;
+}
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int rows = 2;
+int columns = 2;
+int[,] matrixOne = CreateRandomArray(rows, columns, 1, 10);
+ShowArray(matrixOne);
+Console.WriteLine();
+int[,] matrixTwo = CreateRandomArray(rows, columns, 1, 10);
+int[,] resultMatrix = new int[rows,columns];
+ShowArray(matrixTwo);
+Console.WriteLine();
+MultiplyMatrix(matrixOne, matrixTwo, resultMatrix);
+Console.WriteLine("Произведение первой и второй матриц: " );
+ShowArray(resultMatrix);
